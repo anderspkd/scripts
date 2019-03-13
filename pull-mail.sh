399b546501703e6042219322f719b475652c90b9
Anders Dalskov <anderspkd@fastmail.com>
2019-03-13 08:55:27 +0100

    Slightly more intelligent mail script.

--- pull-mail.sh
+++ pull-mail.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-# basically the script from http://www.offlineimap.org/configuration/2016/01/29/why-i-m-not-using-maxconnctions.html
+if [ "$#" -eq 0 ]; then
+    echo "no mailboxes provided. Nothing to do"
+fi
 
-offlineimap -a $1 &
-pid1=$!
+mailboxes=${@:1}
 
-offlineimap -a $2 &
-pid2=$!
+pull-mail () {
+    offlineimap -a $@
+    echo "done at $(date)"
+}
 
-wait $pid1
-wait $pid2
+echo "started at $(date)"
 
-echo "last run $(date)"
+while true
+do
+    found=0
+    read x
+    if [ -z $x ]; then
+	for n in ${mailboxes[@]}
+	do
+	    echo "polling $n ..."
+	    pull-mail $n
+	done
+    else
+	for n in ${mailboxes[@]}
+	do
+	    if [ "$n" = "$x" ]; then
+		echo "polling $x ..."
+		pull-mail $x
+		found=1
+	    fi
+	done
+	if [ "$found" == "0" ]; then
+	    echo "no mailbox found for $x"
+	fi
+    fi
+done
